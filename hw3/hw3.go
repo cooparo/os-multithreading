3a0323eeb33281fbf4816aafac7d306d32f80474
Paro <pparolini1@gmail.com>
2023-08-24 10:57:38 +0200

    hw3.go draft

--- hw3/hw3.go
+++ hw3/hw3.go
@@ -1,14 +1,35 @@
 package main
 
-func cook() {
+import "sync"
+
+type Status int
+
+const (
+	Raw Status = iota
+	Cooked
+	Garnished
+	Decorated
+)
+
+type Cake struct {
+	Status Status
+}
+
+func cook(cake Cake) {
+
+	cake.Status = Cooked
 
 }
 
 func garnish() {
 
+	cake.Status = Garnished
+
 }
 
 func decorate() {
+
+	cake.Status = Decorated
 
 }
 
@@ -16,6 +37,25 @@ func decorate() {
 
 func main() {
 
+	raw_ch := make(chan Cake, 5)
+	cook_ch := make(chan Cake, 1)
+	garnish_ch := make(chan Cake, 1)
+	decorate_ch := make(chan Cake, 1)
+
+	cooked_ch := make(chan Cake, 2) 
+	garnished_ch := make(chan Cake, 2)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go cook(cake)
+	go garnish()
+	go decorate()
+
+	wg.Wait()
+
+	
 	
 
 }
